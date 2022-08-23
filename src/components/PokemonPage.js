428999b8814422b6e8ecbb2378b22d40413b5b3e
Allison Kim <akeem159@yahoo.com>
2022-08-23 14:04:47 -0400

    minor changes

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { useParams } from 'react-router-dom';
-import PokemonCard from "./PokemonCard";
 
 function PokemonPage({ pokemons }) {
 
     const { id } = useParams()
 
     const displayPokemon = pokemons.filter(pokemon => {
-        return pokemon.id == id ? pokemon : null})
+        return pokemon.id === id ? pokemon : null})
 
     return (
         <div>
